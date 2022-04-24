012d3854381c280ee1b390bc24c543bdee0de7a0
ghaerdi <ghaerdi@outlook.com>
2022-04-24 17:06:18 -0400

    feat: use cli args to search

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-mod Search;
 mod globals;
+mod search;
 
-use Search::Vocabulary;
+use search::Vocabulary;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        panic!("to many arguments");
+    }
+
     let mut fruits = Vocabulary::new();
-
     globals::FRUITS.into_iter().for_each(|v| fruits.insert(v));
-    let result = fruits.search("Ap");
 
-    println!("{:#?}", result);
+    let result: Vec<String>;
+
+    match args.get(1) {
+        Some(arg) => {
+            if *arg == ".".to_owned() {
+                result = fruits.get_all();
+            } else {
+                result = fruits.search(arg);
+            }
+        },
+        None => result = fruits.get_all()
+    }
+
+    if result.is_empty() {
+        println!("No results");
+    } else {
+        println!("{:#?}", result);
+    }
+
 }
