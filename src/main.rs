f89c0f6a5b016b67d4c0e75887bc547b113f1b75
ghaerdi <ghaerdi@outlook.com>
2022-04-27 21:00:22 -0400

    run search multiple times to compare three with vecto

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod search;
 
 use search::Vocabulary;
 use std::env;
+use std::time::Instant;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,19 +15,56 @@ fn main() {
     let mut fruits = Vocabulary::new();
     globals::FRUITS.into_iter().for_each(|v| fruits.insert(v));
 
-    let result: Vec<String>;
+    let repeat = 100_000;
 
-    match args.get(1) {
-        Some(arg) => match arg.eq(".") {
-            true => result = fruits.get_all(),
-            false => result = fruits.search(arg),
-        },
-        None => result = fruits.get_all(),
-    }
+    // ARRAY
+    let vector_time = Instant::now();
 
-    if result.is_empty() {
-        println!("No results");
-    } else {
-        println!("{:#?}", result);
-    }
+    (0..repeat).into_iter().for_each(|_| {
+        let result: Vec<&str>;
+
+        match args.get(1) {
+            Some(arg) => match arg.eq(".") {
+                true => result = Vec::from(globals::FRUITS),
+                false => {
+                    result = globals::FRUITS
+                        .into_iter()
+                        .filter(|&el| el.starts_with(arg))
+                        .collect()
+                }
+            },
+            None => result = Vec::from(globals::FRUITS),
+        }
+
+        // if result.is_empty() {
+        //     println!("No results");
+        // } else {
+        //     println!("{:#?}", result);
+        // }
+    });
+
+    println!("ARRAY {}", vector_time.elapsed().as_millis() as f32 / 1000.0);
+
+    // THREE
+    let vocabulary_time = Instant::now();
+
+    (0..repeat).into_iter().for_each(|_| {
+        let result: Vec<String>;
+
+        match args.get(1) {
+            Some(arg) => match arg.eq(".") {
+                true => result = fruits.get_all(),
+                false => result = fruits.search(arg),
+            },
+            None => result = fruits.get_all(),
+        }
+
+        // if result.is_empty() {
+        //     println!("No results");
+        // } else {
+        //     println!("{:#?}", result);
+        // }
+    });
+
+    println!("THREE {}", vocabulary_time.elapsed().as_millis() as f32 / 1000.0)
 }
