6d1d3936896e69ae01363f201b4640a71e8a0134
ghaerdi <ghaerdi@outlook.com>
2022-05-03 01:21:43 -0400

    refactor: using vector to store words in each node for search performance improve

--- src/main.rs
+++ src/main.rs
@@ -12,65 +12,50 @@ fn main() {
         panic!("to many arguments");
     }
 
-    let mut fruits = Vocabulary::new();
-    globals::FRUITS.into_iter().for_each(|v| fruits.insert(v));
-
-    let repeat = 100_000;
+    let repeat = 1_000_000;
 
     // ARRAY
+    let fruits = Vec::from(globals::FRUITS);
+
     let vector_time = Instant::now();
 
-    (0..repeat).into_iter().for_each(|_| {
-        let result: Vec<&str>;
-
+    for _ in 0..repeat {
         match args.get(1) {
-            Some(arg) => match arg.eq(".") {
-                true => result = Vec::from(globals::FRUITS),
-                false => {
-                    result = globals::FRUITS
-                        .into_iter()
-                        .filter(|&el| el.starts_with(arg))
-                        .collect()
-                }
-            },
-            None => result = Vec::from(globals::FRUITS),
-        }
-
-        // if result.is_empty() {
-        //     println!("No results");
-        // } else {
-        //     println!("{:#?}", result);
-        // }
-    });
+            Some(arg) => &fruits
+                .clone()
+                .into_iter()
+                .filter(|&el| el.starts_with(arg))
+                .collect(),
+            None => &fruits,
+        };
+    }
 
     println!(
-        "ARRAY {}",
+        "ARRAY {}s",
         vector_time.elapsed().as_millis() as f32 / 1000.0
     );
 
     // THREE
+    let mut fruits = Vocabulary::new();
+    globals::FRUITS.into_iter().for_each(|v| fruits.insert(v));
+
     let vocabulary_time = Instant::now();
 
     (0..repeat).into_iter().for_each(|_| {
-        let result: Vec<String>;
-
-        match args.get(1) {
-            Some(arg) => match arg.eq(".") {
-                true => result = fruits.get_all(),
-                false => result = fruits.search(arg),
-            },
-            None => result = fruits.get_all(),
-        }
-
-        // if result.is_empty() {
-        //     println!("No results");
-        // } else {
-        //     println!("{:#?}", result);
-        // }
+        fruits.search(args.get(1).unwrap_or(&"".to_owned()));
     });
 
     println!(
-        "THREE {}",
+        "THREE {}s",
         vocabulary_time.elapsed().as_millis() as f32 / 1000.0
-    )
+    );
+
+    let result = fruits.search(args.get(1).unwrap_or(&"".to_owned()));
+
+    match result.is_empty() {
+        true => println!("No results"),
+        false => println!("{:#?}", result),
+    }
+
+    // println!("{:#?}", fruits.len());
 }
--- src/search.rs
+++ src/search.rs
@@ -4,17 +4,23 @@ use std::collections::{HashMap, VecDeque};
 struct Node {
     value: char,
     nodes: HashMap<char, Node>,
-    is_end: bool,
+    words: Vec<String>,
 }
 
 struct NodeBuilder {
     value: char,
     nodes: Vec<Node>,
+    words: Vec<String>,
 }
 
 impl NodeBuilder {
     fn push_node(&mut self, node: Node) -> &mut Self {
         self.nodes.push(node);
+        self
+    }
+
+    fn push_word(&mut self, text: &str)-> &mut Self {
+        self.words.push(text.to_owned());
         self
     }
 
@@ -27,7 +33,7 @@ impl NodeBuilder {
         Node {
             value: self.value,
             nodes: map,
-            is_end: false,
+            words: self.words.clone(),
         }
     }
 }
@@ -37,6 +43,7 @@ impl Node {
         NodeBuilder {
             value: c,
             nodes: vec![],
+            words: vec![],
         }
     }
 
@@ -48,68 +55,55 @@ impl Node {
 #[derive(Debug)]
 pub struct Vocabulary {
     root: Node,
-    len: usize,
 }
 
 impl Vocabulary {
     pub fn new() -> Self {
         Self {
             root: Node::genesis(),
-            len: 0,
         }
     }
     pub fn len(&self) -> usize {
-        self.len
+        self.root.words.len()
     }
 
     pub fn insert(&mut self, text: &str) {
         let chars = Self::str_to_chars(text);
-        let (node, mut chars) = Self::get_node_by_chars(&mut self.root, chars);
-
+        let (node, mut chars) = Self::get_node_by_chars_and_insert(&mut self.root, chars, text);
         match chars.pop_back() {
             Some(c) => {
-                let mut temp = Node::new(c).build();
-
-                temp.is_end = true;
+                let mut temp = Node::new(c).push_word(text).build();
 
                 while let Some(c) = chars.pop_back() {
-                    temp = Node::new(c).push_node(temp).build();
+                    temp = Node::new(c).push_node(temp).push_word(text).build();
                 }
 
                 node.nodes.insert(temp.value, temp);
-                self.len += 1;
             }
-            None => match node.is_end {
+            None => match node.words.contains(&text.to_owned()) {
                 true => println!("{} already exist", text),
                 false => {
-                    node.is_end = true;
-                    self.len += 1;
+                    node.words.push(text.to_owned());
                 }
             },
         }
     }
 
-    pub fn search(&mut self, text: &str) -> Vec<String> {
+    pub fn search(&self, text: &str) -> &Vec<String> {
         if text.is_empty() {
-            return vec![];
+            return self.get_all();
         }
 
         let text = text.to_lowercase();
 
         let chars = Self::str_to_chars(&text);
-        let (node, chars) = Self::get_node_by_chars(&mut self.root, chars);
+        let (node, _) = Self::get_node_by_chars(&self.root, chars);
 
-        if !chars.is_empty() {
-            return vec![];
-        }
-
-        Self::nodes_to_text(&text, node.clone())
+        &node.words
     }
 
-    pub fn get_all(&mut self) -> Vec<String> {
-        let chars = Self::str_to_chars("");
-        let (node, _) = Self::get_node_by_chars(&mut self.root, chars);
-        Self::nodes_to_text("", node.clone())
+    pub fn get_all(&self) -> &Vec<String> {
+        &self.root.words
     }
 
     fn str_to_chars(s: &str) -> VecDeque<char> {
@@ -122,65 +116,35 @@ impl Vocabulary {
         chars
     }
 
-    fn get_node_by_chars(
-        node: &mut Node,
+    fn get_node_by_chars_and_insert<'a>(
+        node: &'a mut Node,
         mut chars: VecDeque<char>,
-    ) -> (&mut Node, VecDeque<char>) {
+        text: &str,
+    ) -> (&'a mut Node, VecDeque<char>) {
         if let Some(c) = chars.pop_front() {
-            match node.nodes.contains_key(&c) {
-                true => return Self::get_node_by_chars(node.nodes.get_mut(&c).unwrap(), chars),
-                false => {
-                    chars.push_front(c);
-                    return (node, chars);
-                }
+            node.words.push(text.to_owned());
+            if node.nodes.contains_key(&c) {
+                return Self::get_node_by_chars_and_insert(
+                    node.nodes.get_mut(&c).unwrap(),
+                    chars,
+                    text,
+                );
             }
+            chars.push_front(c);
+            return (node, chars);
         }
+
         (node, chars)
     }
 
-    fn nodes_to_text(text: &str, node: Node) -> Vec<String> {
-        struct TempNode {
-            node: Node,
-            text: String,
-        }
-
-        let mut stack = vec![];
-        let mut words = vec![];
-
-        node.nodes.into_iter().for_each(|(_, el)| {
-            stack.push(TempNode {
-                node: el,
-                text: text.to_string(),
-            })
-        });
-
-        fn recursive(stack: &mut Vec<TempNode>, words: &mut Vec<String>) {
-            if stack.is_empty() {
-                return;
+    fn get_node_by_chars(node: &Node, mut chars: VecDeque<char>) -> (&Node, VecDeque<char>) {
+        if let Some(c) = chars.pop_front() {
+            if node.nodes.contains_key(&c) {
+                return Self::get_node_by_chars(node.nodes.get(&c).unwrap(), chars);
             }
-
-            let current = stack.pop().unwrap();
-            let text = format!("{}{}", current.text, current.node.value);
-
-            match !current.node.nodes.is_empty() {
-                true => current.node.nodes.into_iter().for_each(|(_, el)| {
-                    if current.node.is_end {
-                        words.push(text.clone());
-                    }
-
-                    stack.push(TempNode {
-                        node: el,
-                        text: text.clone(),
-                    });
-                }),
-                false => words.push(text),
-            }
-
-            recursive(stack, words)
+            chars.push_front(c);
+            return (node, chars);
         }
-
-        recursive(&mut stack, &mut words);
-
-        words
+        (node, chars)
     }
 }
